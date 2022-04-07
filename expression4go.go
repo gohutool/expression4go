883d4f3c77cf708b4980dddbdd9231edf3a11fda
LiuYong <793875613@qq.com>
2022-04-07 22:13:01 +0800

    add version info for expression4go.go

--- expression4go.go
+++ expression4go.go
@@ -3,7 +3,7 @@ package expression4go
 /**
 * golang-sample源代码，版权归锦翰科技（深圳）有限公司所有。
 * <p>
-* 文件名称 : spelExpressionParser.go
+* 文件名称 : expression4go.go
 * 文件路径 :
 * 作者 : DavidLiu
 × Email: david.liu@ginghan.com
@@ -11,6 +11,13 @@ package expression4go
 * 创建日期 : 2022/4/7 18:05
 * 修改历史 : 1. [2022/4/7 18:05] 创建文件 by NST
 */
+
+const (
+	L4G_VERSION = "expression4go-v1.0.0"
+	L4G_MAJOR   = 1
+	L4G_MINOR   = 0
+	L4G_BUILD   = 0
+)
 
 type SpelExpressionParser struct {
 	*TemplateAwareExpressionParser
